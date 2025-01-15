0f80e83ebf9bb3c2cabd8089e49fa599e5228043
Diana Benish <dianaal791@gmail.com>
2025-01-15 09:56:45 +0200

    Update addVolunteerRequests.js

--- backend/script/addVolunteerRequests.js
+++ backend/script/addVolunteerRequests.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const VolunteerLocation = require('../models/volunteerLocation');
 
-mongoose.connect('mongodb+srv://dianaal791:<dianush11220>@dianab.dd568.mongodb.net/?retryWrites=true&w=majority&appName=dianaB', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {

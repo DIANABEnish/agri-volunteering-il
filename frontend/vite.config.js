5ffb1f0d0014945aaf368bb07dcffba0bff675c0
Diana Benish <dianaal791@gmail.com>
2025-01-15 22:17:21 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
-      usePolling: true, // Enable polling to watch for changes
+      usePolling: true,
     },
     proxy: {
-      '/api': 'http://localhost:3003', // Proxy API requests to your backend server
-      
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:3003',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
 });

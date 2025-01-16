52336bd1b5e9a6cb7a9b9c75918a88c1f6beafc0
Diana Benish <dianaal791@gmail.com>
2025-01-16 11:29:17 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   server: {
     watch: {
       usePolling: true,

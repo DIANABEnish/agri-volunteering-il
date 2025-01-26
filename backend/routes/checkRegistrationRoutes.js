b531bce27bb06414d7e3d4aafaeda1a31aa90be4
Diana Benish <dianaal791@gmail.com>
2025-01-26 15:24:18 +0200

    Delete backend directory

--- backend/routes/checkRegistrationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkRegistration } = require('../controllers/checkRegistrationController');
-
-router.get('/:locationId/:email', checkRegistration);
-
-module.exports = router;
